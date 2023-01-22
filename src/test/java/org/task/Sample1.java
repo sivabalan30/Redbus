0e76a59335006ff5524a156fc544d887badb7c26
sivabalan30 <sivabk1996@gmail>
2023-01-22 23:46:01 +0530

    my project

---
+++ src/test/java/org/task/Sample1.java
@@ -0,0 +1,14 @@
+package org.task;
+
+public class Sample1 {
+
+	private void praveen() {
+    System.out.println("praveen");
+	}
+	private void bruce() {
+	    System.out.println("bruce");
+		}
+	private void tamil() {
+	    System.out.println("tamil");
+		}
+}
---
+++ src/test/java/org/task/Sample2.java
@@ -0,0 +1,14 @@
+package org.task;
+
+public class Sample2 {
+
+	private void india() {
+    System.out.println("india");
+	}
+	private void paki() {
+	    System.out.println("paki");
+		}
+	private void mount() {
+		    System.out.println("mount");
+		}
+}
